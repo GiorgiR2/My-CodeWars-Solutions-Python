09ff308f7d8d34b0ce05b09f628fbf5123e674c3
GiorgiR <25549786+GiorgioRR@users.noreply.github.com>
2019-09-12 18:07:17 +0400

    Update Grill it!.py

--- 6 kyu/Grill it!.py
+++ 6 kyu/Grill it!.py
@@ -1,12 +1,7 @@
 # https://www.codewars.com/kata/grill-it/train/python
 def grille(message, code):
-    answer, code = "", "{0:0{1}b}".format(code, len(message))
-    if len(code) > len(message):
-        code = code[int("-" + str(len(message))):]
-    for i, j in zip(list(message), list(code)):
-        if j == "1":
-            answer += i
-    return answer
+    code = "{0:0{1}b}".format(code, len(message))[int("-" + str(len(message))):]
+    return "".join([i for i, j in zip(list(message), list(code)) if j == "1"])
 
 
 print(grille("ab", 256) == "")
